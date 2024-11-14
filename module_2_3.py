c7a726da89a03ab7ce4a9b721eb8fa180c05a579
Han-Mandyk <hensilej@gmail.com>
2024-11-14 19:10:16 +0800

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-numbers = 0
-while numbers < len(my_list):
-    if my_list[numbers] < numbers:
-        my_list.remove(my_list[numbers])
-    else:
-        numbers += 1
-print(my_list)
+numbers = []
+i = 0
+
+while i < len(my_list):
+    if my_list[i] < 0:
+        break
+    numbers.append(my_list[i])
+    i += 1
+
+print(numbers)
