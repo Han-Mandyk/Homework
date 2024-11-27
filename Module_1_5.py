7c7aef5b2a7fce6838d120da9a022e0d9b465565
Han-Mandyk <hensilej@gmail.com>
2024-11-27 18:00:24 +0800

    Add files via upload

---
+++ Module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 45, 56.5, 'String', True
+#immutable_var[0] = 12      кортеж своего рода неизменяемая константа и из-за нам запрещается менять ее значение
+mutable_list = ['Dinis', 5, True]
+mutable_list[2] = False
+print(immutable_var)
+print(mutable_list)
